69d84738b33cc8a6ae96305343f3f085fd317b46
afperdomo2 <alvarado0925@gmail.com>
2023-06-09 14:48:57 -0500

    Usando rutas con varios parámetros

--- index.js
+++ index.js
@@ -9,9 +9,25 @@ app.get("/home", (req, res) => {
   res.send("Home");
 });
 app.get("/products", (req, res) => {
+  res.json([
+    { name: "Pantalon", price: 1500 },
+    { name: "Pera", price: 10 },
+  ]);
+});
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  res.json({ id, name: "Pantalon", price: 1500 });
+});
+
+app.get("/categories/:categoryId/products/:productId", (req, res) =>{
+  const {categoryId, productId} = req.params;
   res.json({
-    name: "Pantalon",
-    price: 1500
+    categoryId,
+    product: {
+      productId,
+      name: "Pantalon",
+      price: 1500
+    }
   });
 });
 
