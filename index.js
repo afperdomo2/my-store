d0b343caedd198b22aec0ca7315d2c6ef2e5401a
afperdomo2 <alvarado0925@gmail.com>
2023-06-09 14:19:45 -0500

    Crea un servidor básico con express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(123123);
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("Servidor en express")
+});
+
+app.listen(port, () => {
+  console.info(`✅ Server express on: http://localhost:${port}`)
+})
