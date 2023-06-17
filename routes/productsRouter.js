26c5489bbd4aa81e9de6c69e95386255c37d54ef
afperdomo2 <alvarado0925@gmail.com>
2023-06-17 08:53:53 -0500

    Implementa los servicios para productRouter

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,92 +1,58 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+
+const ProductsService = require('../services/productsService');
 
 const router = express.Router();
+const service = new ProductsService();
 
 // POST
 router.post('/', (req, res) => {
   const body = req.body;
-  res.status(201).json({
-    message: 'created',
-    data: body,
-  });
+  const newProduct = service.create(body);
+  res.status(201).json(newProduct);
 });
 
 // PUT -  Se usa para reemplazar completamente un recurso
-router.put('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  res.status(200).json({
-    message: 'updated',
-    id,
-    data: body,
-  });
-});
+// router.put('/:id', (req, res) => {
+//   const { id } = req.params;
+//   const body = req.body;
+//   res.status(200).json({
+//     message: 'updated',
+//     id,
+//     data: body,
+//   });
+// });
 
 // PATCH - Se usa para parchar o actualizar parcialmente el recurso
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
-  if (body.name === undefined) {
-    res.status(500).json({
-      message: 'Server Error',
-    });
-  }
-  /**
-   * En PUT, PATCH y DELETE, también se puede usar el status 204, que
-   * indica que el servidor ha completado exitosamente la solicitud y
-   * no hay contenido para enviar en el cuerpo de la respuesta
-   */
-  if (id) {
-    res.status(204).json();
-  }
+  const product = service.update(id, body);
+  res.status(200).json(product);
 });
 
 // DELETE
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  res.status(200).json({
-    message: 'deleted',
-    id,
-  });
+  service.delete(id);
+  /**
+   * En PUT, PATCH y DELETE, también se puede usar el status 204, que
+   * indica que el servidor ha completado exitosamente la solicitud y
+   * no hay contenido para enviar en el cuerpo de la respuesta
+   */
+  res.status(204).json();
 });
 
 // GET
 router.get('/', (req, res) => {
-  const { size } = req.query;
-
-  const products = [];
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      id: index + 1,
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url(),
-    });
-  }
+  const products = service.find();
   res.status(200).json(products);
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-
-  const products = [];
-  for (let index = 0; index < 10; index++) {
-    products.push({
-      id: index + 1,
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url(),
-    });
-  }
-  const product = products.filter((p) => p.id === parseInt(id));
-  if (product.length === 0) {
-    res.status(404).json({
-      message: 'Not Found',
-    });
-  }
-  res.status(200).json(product[0]);
+  const product = service.findOne(id);
+  res.status(200).json(product);
 });
 
 module.exports = router;
---
+++ services/productsService.js
@@ -0,0 +1,60 @@
+const { faker } = require('@faker-js/faker');
+
+class ProductsService {
+  constructor() {
+    this.products = [];
+    this.generate();
+  }
+
+  create(data) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      ...data,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  find() {
+    return this.products;
+  }
+
+  findOne(id) {
+    return this.products.find((p) => p.id === parseInt(id));
+  }
+
+  update(id, changes) {
+    const index = this.products.findIndex((p) => p.id === parseInt(id));
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    const product = this.products[index];
+    this.products[index] = {
+      ...product,
+      ...changes,
+    };
+    return this.products[index];
+  }
+
+  delete(id) {
+    const index = this.products.findIndex((p) => p.id === parseInt(id));
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    this.products.splice(index, 1);
+    return id;
+  }
+
+  generate() {
+    for (let index = 0; index < 100; index++) {
+      this.products.push({
+        id: index + 1,
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(), 10),
+        image: faker.image.url(),
+      });
+    }
+  }
+}
+
+module.exports = ProductsService;
