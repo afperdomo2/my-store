cde1c9af3503e7b82b286e6ff07fc3f48dfec7c9
afperdomo2 <alvarado0925@gmail.com>
2023-07-04 11:40:53 -0500

    Instala e implementa el Middleware de CORS

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
+const path = require('path');
 
 const {
   logErrors,
@@ -8,7 +10,12 @@ const {
 } = require('./middlewares/errorHandler');
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
+const STATIC_PORT = 4000;
+
+// STATIC FILES
+const staticDir = path.join(__dirname, 'public');
+app.use(express.static(staticDir));
 
 /**
  * express.json() es una función en Express que te permite analizar
@@ -16,16 +23,34 @@ const port = 3000;
  * un objeto JavaScript que puedes usar en tu aplicación.
  */
 app.use(express.json());
+
+// CORS
+const whiteList = ['http://localhost:8080', 'http://localhost:4000'];
+const options = {
+  origin: (origin, callback) => {
+    if (!whiteList.includes(origin)) {
+      callback(new Error('Acceso no permitido'));
+    }
+    callback(null, true);
+  },
+};
+app.use(cors(options));
+
+// ROUTES
 routerApi(app);
 
 /**
- * Ejecución de los middlewares
+ * MIDDLEWARES
  * Es importante tener en cuenta el orden de ejecución
  */
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.info(`✅ Server express on: http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.info(`✅ Server express on: http://localhost:${PORT}`);
+});
+
+app.listen(STATIC_PORT, () => {
+  console.log(`✅ Static Dir on: http://localhost:${STATIC_PORT}`);
 });
