2d7efcbda19cdd170217af1307a1c33dfc788958
afperdomo2 <alvarado0925@gmail.com>
2023-07-21 17:28:45 -0500

    Instala sequelize-cli y hace la configuración inicial de las migraciones

--- db/config.js
+++ db/config.js
@@ -1,15 +1,17 @@
-const { Sequelize } = require('sequelize');
 const { config } = require('../config/config');
-const setupModels = require('../db/models/index');
 
 const { dbDialect: DIALECT, dbHost: HOST, dbPort: PORT, dbName: NAME } = config;
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `${DIALECT}://${USER}:${PASSWORD}@${HOST}:${PORT}/${NAME}`;
 
-const sequelize = new Sequelize(URI, { dialect: DIALECT, logging: true });
-
-setupModels(sequelize);
-sequelize.sync();
-
-module.exports = sequelize;
+module.exports = {
+  development: {
+    url: URI,
+    dialect: DIALECT,
+  },
+  production: {
+    url: URI,
+    dialect: DIALECT,
+  },
+};
